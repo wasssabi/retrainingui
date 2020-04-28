d6cbbea8da4a216da3aba49371c41c870bbb5f30
annakitchak <annakitchak@gmail.com>
2020-04-28 15:06:29 +0300

    fields filter and search added

---
+++ src/app/fields/display-fields/display-fields.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-fields',
+  templateUrl: './display-fields.component.html',
+  styleUrls: ['./display-fields.component.css']
+})
+export class DisplayFieldsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/fields/fields.module.ts
+++ src/app/fields/fields.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FieldsComponent } from "./fields.component";
+import { FieldsFilterComponent } from './fields-filter/fields-filter.component';
+import { DisplayFieldsComponent } from './display-fields/display-fields.component';
+import { SearchFieldsComponent } from './search-fields/search-fields.component';
 
 @NgModule({
-  declarations: [ FieldsComponent],
+  declarations: [ FieldsComponent, FieldsFilterComponent, DisplayFieldsComponent, SearchFieldsComponent],
   imports: [
     CommonModule
   ]
