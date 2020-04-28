23b2c09bf6b60d58cc58b9fbdb7d63b3c016b207
annakitchak <annakitchak@gmail.com>
2020-04-28 21:47:15 +0300

    show Fields, Fields Filter,Fields Search

--- src/app/fields/fields.module.ts
+++ src/app/fields/fields.module.ts
@@ -5,11 +5,15 @@ import { FieldsComponent } from "./fields.component";
 import { FieldsFilterComponent } from './fields-filter/fields-filter.component';
 import { DisplayFieldsComponent } from './display-fields/display-fields.component';
 import { SearchFieldsComponent } from './search-fields/search-fields.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [ FieldsComponent, FieldsFilterComponent, DisplayFieldsComponent, SearchFieldsComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
   ]
 })
 export class FieldsModule { }
