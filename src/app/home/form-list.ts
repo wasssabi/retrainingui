04e8220c93b1f020a61eb06524451f1c878cd664
mplesha <tes.t@ukr.net>
2020-05-01 11:19:24 +0000

    gh-pages, start

--- src/app/home/form-list.ts
+++
@@ -1,9 +0,0 @@
-import { Form } from "./form";
-
-export const FormList: Form[] =
-[
-  {id:1, name:'form_item1', description:'description1'},
-  {id:2, name:'item2', description:'description2'},
-  {id:3, name:'item3', description:'description3'},
-  {id:4, name:'item4', description:'description4'}
-]
--- src/app/home/form.ts
+++
@@ -1,5 +0,0 @@
-export interface Form {
-  id: number;
-  name: string;
-  description: string;
-}
