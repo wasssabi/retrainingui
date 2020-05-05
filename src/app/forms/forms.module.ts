09516b51339b9eb5e59d1c175f47fd32121421b7
annakitchak <annakitchak@gmail.com>
2020-05-05 15:46:54 +0300

    fields search implemented, forms search component created

--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { FormsComponent } from "./forms.component";
 import { FormsFilterComponent } from './forms-filter/forms-filter.component';
 import { FormsItemComponent } from './forms-item/forms-item.component';
+import { SearchFormsComponent } from './search-forms/search-forms.component';
 
 @NgModule({
-  declarations: [FormsComponent, FormsFilterComponent, FormsItemComponent],
+  declarations: [FormsComponent, FormsFilterComponent, FormsItemComponent, SearchFormsComponent],
   imports: [
     CommonModule
   ],
